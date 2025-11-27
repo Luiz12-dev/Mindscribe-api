bb65495659d00584928438b0390b8ad4eb8104ce
Luiz12-dev <aluizotaviodealmeida@gmail.com>
2025-11-27 14:07:44 -0300

    refactor(flyway):corrigindo erro de digitação

--- src/main/resources/db/migration/VI__create_tables_users_and_notes.sql
+++ src/main/resources/db/migration/VI__create_tables_users_and_notes.sql
@@ -1,5 +1,5 @@
 CREATE TABLE tb_users (
-    id UUID DEFAULT gen_randon_uuid PRIMARY KEY,
+    id UUID DEFAULT gen_random_uuid PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR (255) NOT NULL,
@@ -7,7 +7,7 @@ CREATE TABLE tb_users (
 );
 
 CREATE TABLE tb_notes (
-    id UUID DEFAULT gen_randon_uuid PRIMARY KEY,
+    id UUID DEFAULT gen_random_uuid PRIMARY KEY,
     title VARCHAR(200) NOT NULL,
     content TEXT, 
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
