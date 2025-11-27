ddfa8fef89cfb8efe01f97735b7c0b873ce34e29
Luiz12-dev <aluizotaviodealmeida@gmail.com>
2025-11-27 14:15:10 -0300

    feat(domain): cria atributo de relacionamento com a entidade Note e cria o atributo updatedAt

--- src/main/java/br/com/evernot/project/domain/UserEntity.java
+++ src/main/java/br/com/evernot/project/domain/UserEntity.java
@@ -1,17 +1,21 @@
 package br.com.evernot.project.domain;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -47,8 +51,15 @@ public class UserEntity {
     @Column(nullable = false)
     private String password;
 
+    @OneToMany(mappedBy = "user",fetch = FetchType.LAZY)
+    private List<NoteEntity> notes;
+
     @CreationTimestamp
-    @Column(nullable = false)
-    private LocalDateTime created_at;
+    @Column(name = "created_at",nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
 }
