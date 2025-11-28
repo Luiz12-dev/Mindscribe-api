e7517704ff43ba6ff7015cf1c4880d45cee56dad
Luiz12-dev <aluizotaviodealmeida@gmail.com>
2025-11-28 16:42:08 -0300

    feat(repository): cria metodo de procura no banco de dados

--- src/main/java/br/com/evernot/project/repository/NoteRepository.java
+++ src/main/java/br/com/evernot/project/repository/NoteRepository.java
@@ -1,5 +1,6 @@
 package br.com.evernot.project.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.evernot.project.domain.NoteEntity;
 
 public interface NoteRepository extends JpaRepository<NoteEntity, UUID>{
+
+    List<NoteEntity> findAllByUserId(UUID id);
     
 }
