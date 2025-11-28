6279a03f7b6b4ddf6a54fdecef0ba430bb25d536
Luiz12-dev <aluizotaviodealmeida@gmail.com>
2025-11-28 16:40:38 -0300

    feat(dto): cria metodo auxiliar para transformar NoteEntity em um de response

--- src/main/java/br/com/evernot/project/dto/NoteResponseDto.java
+++ src/main/java/br/com/evernot/project/dto/NoteResponseDto.java
@@ -3,12 +3,23 @@ package br.com.evernot.project.dto;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import br.com.evernot.project.domain.NoteEntity;
+
 public record NoteResponseDto(
     UUID id,
     String title,
     String content,
-    LocalDateTime created_at,
-    LocalDateTime updated_at
-    
+    LocalDateTime createdAt,
+    LocalDateTime updatedAt
+) {
 
-) {}
+    public static NoteResponseDto sucess(NoteEntity note){
+        return new NoteResponseDto(
+            note.getId(),
+            note.getTitle(),
+            note.getContent(),
+            note.getCreatedAt(),
+            note.getUpdatedAt()
+        );
+    }
+}
