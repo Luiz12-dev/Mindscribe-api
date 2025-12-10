8e224494995a029f2490339030824c95d27dea21
Luiz12-dev <aluizotaviodealmeida@gmail.com>
2025-12-10 14:16:58 -0300

    chore(dto): remove metodo auxiliar para trasnformar em dto

--- src/main/java/br/com/evernot/project/dto/NoteResponseDto.java
+++ src/main/java/br/com/evernot/project/dto/NoteResponseDto.java
@@ -3,7 +3,6 @@ package br.com.evernot.project.dto;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import br.com.evernot.project.domain.NoteEntity;
 
 public record NoteResponseDto(
     UUID id,
@@ -12,14 +11,4 @@ public record NoteResponseDto(
     LocalDateTime createdAt,
     LocalDateTime updatedAt
 ) {
-
-    public static NoteResponseDto sucess(NoteEntity note){
-        return new NoteResponseDto(
-            note.getId(),
-            note.getTitle(),
-            note.getContent(),
-            note.getCreatedAt(),
-            note.getUpdatedAt()
-        );
-    }
 }
