e7e810be5caf5b6c02811f39779bb6d317791b20
Luiz12-dev <aluizotaviodealmeida@gmail.com>
2025-12-10 14:14:49 -0300

    feat(repository): cria metodo para visualizar todas as notas de um indivíduo

--- src/main/java/br/com/evernot/project/service/NoteService.java
+++ src/main/java/br/com/evernot/project/service/NoteService.java
@@ -1,5 +1,6 @@
 package br.com.evernot.project.service;
 
+import java.util.List;
 import java.util.UUID;
 
 import br.com.evernot.project.dto.NoteRequestDto;
@@ -8,5 +9,7 @@ import br.com.evernot.project.dto.NoteResponseDto;
 public interface NoteService {
 
     NoteResponseDto createNote(NoteRequestDto req, UUID Id);
+
+    List<NoteResponseDto> userNotes(UUID id);
 
 }
