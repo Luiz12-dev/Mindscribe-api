ca3dc80ca3d2ce5ef812fd0f87db1fe13548557c
Luiz12-dev <aluizotaviodealmeida@gmail.com>
2026-01-13 14:02:06 -0300

    feat(Security): Adiciona injeção da classe SecurityFilter, Implemnta o gerenciamento da sessão, implementa o gerenciamento das rotas, implementa gerenciador de autenticação e BCryp

--- src/main/java/br/com/evernot/project/security/config/SecurityConfig.java
+++ src/main/java/br/com/evernot/project/security/config/SecurityConfig.java
@@ -2,24 +2,57 @@ package br.com.evernot.project.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    
+    private final SecurityFilter securityFilter;
+    
+
+    public SecurityConfig(SecurityFilter securityFilter) {
+        this.securityFilter = securityFilter;
+    }
+
+
     @Bean 
     public SecurityFilterChain filterChain (HttpSecurity http)throws Exception{
         http
-        .csrf(AbstractHttpConfigurer::disable)
-        .cors(AbstractHttpConfigurer::disable)
-        .authorizeHttpRequests(auth -> auth
-            .anyRequest().permitAll()
-        );
-        return http.build();
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                .requestMatchers(HttpMethod.POST,"/users").permitAll()
+                .anyRequest().authenticated()
+            )
+            .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+
+            return http.build();
+    }
+
+   
+    @Bean
+    public AuthenticationManager authentiuAuthenticationManager(AuthenticationConfiguration authenticationConfiguration)throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
+    @Bean
+     public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
