c9b808f56e3566a46de247b782388f7c6f222363
Luiz12-dev <aluizotaviodealmeida@gmail.com>
2026-07-07 16:42:41 -0300

    refactor(controller): remove unused GetMapping and clean up UserController

--- src/main/java/br/com/evernot/project/controller/UserController.java
+++ src/main/java/br/com/evernot/project/controller/UserController.java
@@ -2,7 +2,6 @@ package br.com.evernot.project.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,9 +30,6 @@ public class UserController {
         return new ResponseEntity<>(res, HttpStatus.CREATED);
     }
 
-    @GetMapping
-    public String teste(){
-        return "EndpointFuncioanndo corretamente !";
-    }
+    
 
 }
